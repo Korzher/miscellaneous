f2676868e7438d877559ad0a7a7ae545fc7fcbf9
Korzher <tchuprinin.constantin@yandex.ru>
2025-05-14 00:03:13 +0300

    PHP practice

---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h3>GET-форма:</h3>
+<form action="/result.php" method="GET">
+	<input name="test1">
+	<input name="test2">
+	<input type="submit">
+</form>
+
+<h3>POST-форма:</h3>
+<form action="/result.php" method="POST">
+	<input name="test1">
+	<input name="test2">
+	<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ PHP/result.php
@@ -0,0 +1,11 @@
+<!-- result.php -->
+<!DOCTYPE html>
+<html>
+<body>
+
+<h3>Результаты:</h3>
+<p>GET-данные: <?= htmlspecialchars($_GET['get_data'] ?? '') ?></p>
+<p>POST-данные: <?= htmlspecialchars($_POST['post_data'] ?? '') ?></p>
+
+</body>
+</html>
