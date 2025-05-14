2872d3cbb04bea5afe0fab89fb00ae491dc17e92
Korzher <tchuprinin.constantin@yandex.ru>
2025-05-14 11:39:02 +0300

    PHP practice

--- PHP/index.php
+++ PHP/index.php
@@ -3,16 +3,18 @@
 <body>
 
 <h3>GET-форма:</h3>
+<p>Направьте числа для сложения</p>
 <form action="/result.php" method="GET">
-	<input name="test1">
-	<input name="test2">
+	<input name="number1">
+	<input name="number2">
 	<input type="submit">
 </form>
 
 <h3>POST-форма:</h3>
+<p>Направьте числа для вычитания</p>
 <form action="/result.php" method="POST">
-	<input name="test1">
-	<input name="test2">
+	<input name="number1">
+	<input name="number2">
 	<input type="submit">
 </form>
 
--- PHP/result.php
+++ PHP/result.php
@@ -3,8 +3,17 @@
 <body>
 
 <h3>Результаты:</h3>
-<p>GET-данные: <?= htmlspecialchars($_GET['get_data'] ?? '') ?></p>
-<p>POST-данные: <?= htmlspecialchars($_POST['post_data'] ?? '') ?></p>
-
+<p>GET-данные: <?php 
+    if (isset($_GET['number1']) && isset($_GET['number2'])) {
+    echo $_GET['number1'] + $_GET['number2'];
+    } else {
+    echo '0';
+    } ?></p>
+<p>POST-данные: <?php 
+    if (isset($_POST['number1']) && isset($_POST['number2'])) {
+    echo $_POST['number1'] - $_POST['number2'] ;
+     } else {
+    echo '0';
+    } ?></p>
 </body>
 </html>
