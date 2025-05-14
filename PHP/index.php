2c4e71c1ea430a5d85940eb65800c9debcd34caa
Korzher <tchuprinin.constantin@yandex.ru>
2025-05-14 12:32:42 +0300

    PHP practice

--- PHP/index.php
+++ PHP/index.php
@@ -18,5 +18,17 @@
 	<input type="submit">
 </form>
 
+<h3>POST-форма информация о пользователе </h3>
+
+<form action="/result.php" method="POST">
+    <input type="text" name="name" placeholder="name"><br>
+    <input type="text" name="age" placeholder="age"><br>
+    <input type="password" name="pass" placeholder="password"><br>
+    <input type="text" name="YofB" placeholder="Year of Birth"><br>
+    <input type="text" name="MofB" placeholder="Month of Birth"><br>
+    <input type="text" name="DofB" placeholder="Day of Birth"><br>
+    <input type="submit">
+</form>
+
 </body>
 </html>
--- PHP/result.php
+++ PHP/result.php
@@ -3,17 +3,42 @@
 <body>
 
 <h3>Результаты:</h3>
+<?php if (isset($_GET['number1'])) { ?>
 <p>GET-данные: <?php 
     if (isset($_GET['number1']) && isset($_GET['number2'])) {
     echo $_GET['number1'] + $_GET['number2'];
     } else {
     echo '0';
     } ?></p>
-<p>POST-данные: <?php 
+<?php } ?>
+
+<?php if (isset($_POST['number1'])) { ?>
+    <p>POST-данные: <?php 
     if (isset($_POST['number1']) && isset($_POST['number2'])) {
     echo $_POST['number1'] - $_POST['number2'] ;
      } else {
     echo '0';
     } ?></p>
+    <?php } ?>
+
+    <?php if(isset($_POST['name'])) { ?>
+    <?php $pass = "12345"?>
+    <p> Имя пользователя: <?= $_POST['name'] ?></p> <br>
+    <p> Возраст пользователя: <?= $_POST['age'] ?></p> <br>
+    <p> Дата рождения пользователя: <?= $_POST['DofB'] . "." . $_POST['MofB'] . "." . $_POST['YofB']?></p> <br>
+    <p> День недели, когда родился пользователь: <?php
+        $date = $_POST['YofB'] . "-" . $_POST['MofB'] . "-" . $_POST['DofB'];
+        echo date('l', strtotime($date));
+     ?></p> <br>
+    <p> Пароль совпадает: <?php 
+        if($pass === $_POST['pass']) {
+            echo 'true';
+        } else {
+            echo 'false';
+        }?></p>
+    <?php } ?>
+
+
+
 </body>
 </html>
